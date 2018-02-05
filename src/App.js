be168b9d17c3e1a95b5cf0f3e426752a0a114d9a
Vlad <vladislavholovko@gmail.com>
2018-02-05 17:09:04 +0200

    Change Logic

--- src/App.js
+++ src/App.js
@@ -9,18 +9,19 @@ import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 const BasicExample = () => (
   <Router>
-    <div>
-      <ul>
-        <li><Link to="/">MyBlog</Link></li>
-        <li><Link to="/galeria">Galeria</Link></li>
-        <li><Link to="/users">Users</Link></li>
-      </ul>
-      
-      <hr/>
-      
-      <Route exact path="/" component={MyBlog}/>
-      <Route path="/galeria" component={Galeria}/>
-      <Route path="/users" component={Users}/>
+    <div className = "Container">
+      <div className = "MenuContainer">
+          <ul className = "MenuLists">
+            <li><Link className = "List" to="/">MyBlog</Link></li>
+            <li><Link className = "List" to="/galeria">Galeria</Link></li>
+            <li><Link className = "List" to="/users">Users</Link></li>
+          </ul>
+        </div>
+        <hr/>
+        
+        <Route exact path="/" component={MyBlog}/>
+        <Route path="/galeria" component={Galeria}/>
+        <Route path="/users" component={Users}/>
     </div>
   </Router>
 )
